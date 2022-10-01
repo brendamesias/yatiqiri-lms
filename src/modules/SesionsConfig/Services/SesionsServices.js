69198398d262a28f0bf46e90b7438203809b9ee9
brendamesias <yatiqiri.peru@gmail.com>
2022-10-01 03:53:08 -0500

    remove comment

--- src/modules/SesionsConfig/Services/SesionsServices.js
+++ src/modules/SesionsConfig/Services/SesionsServices.js
@@ -2,14 +2,6 @@ import { getStorage, ref, uploadBytes } from "firebase/storage";
 import {db} from "../../../firebase"
 import { addDoc, collection } from "firebase/firestore";
 
-// cursos / react / sesiones / hooks (descipcion, video, links, link calendly, link feedback, asignación, cuestionario tecnico)
-// cursos / react / profesores / idDelprofesor 
-// cursos / react / feedback del curso en general
-// cursos / react / proyecto del curso
-
-// profesores / brenda / cursos [arrays de id de los cursos y dentor de cada curso las sesiones]
-// alumnos
-//usuarios/ idDeUsuario/ objeto con estos datos (rol estudiante , admin o profesor / tipo de bootcamp frontend o backend o qa / cursos matriculados )
 
 export const uploadFileToSesion = async(courseName, sessionName, doc) => {
     //specificar en el name, si es documento, o imagen por ejemplo: doc_test-hooks o img-hooks
